0ab70fbabb5a22689a309e2b85adb7b3e6fe53f3
Muhammad Al Biruni <birunimamak@gmail.com>
2021-10-05 05:55:02 +0700

    Tugas 1

---
+++ app/src/main/java/com/example/pertemuan_1_intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pertemuan_1_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void biodata(View view){
+        Intent a = new Intent(MainActivity.this,
+                Biodata.class);
+        startActivity(a);
+    }
+
+    public void segitiga(View view){
+        Intent a = new Intent(MainActivity.this,
+                Segitiga.class);
+        startActivity(a);
+    }
+}
